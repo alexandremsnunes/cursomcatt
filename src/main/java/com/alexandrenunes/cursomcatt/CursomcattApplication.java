7e277df9746b279cb82099c2d34fd8f06794bb05
alexandremsnunes <msn.alexandre@gmail.com>
2021-03-09 19:59:40 -0300

    Criando Projeto

---
+++ src/main/java/com/alexandrenunes/cursomcatt/CursomcattApplication.java
@@ -0,0 +1,13 @@
+package com.alexandrenunes.cursomcatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcattApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandrenunes/cursomcatt/CursomcattApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandrenunes.cursomcatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
