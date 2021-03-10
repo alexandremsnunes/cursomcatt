535d47562f471891b41dcff18db53b7c541e5746
alexandremsnunes <msn.alexandre@gmail.com>
2021-03-10 17:21:09 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/alexandrenunes/cursomcatt/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.alexandrenunes.cursomcatt.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandrenunes.cursomcatt.domain.Cliente;
+import com.alexandrenunes.cursomcatt.repositories.ClienteRepository;
+import com.alexandrenunes.cursomcatt.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
