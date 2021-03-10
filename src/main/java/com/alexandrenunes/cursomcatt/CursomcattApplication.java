69f4aa8fdae0d0f5cb8f53052c48cda06697e440
alexandremsnunes <msn.alexandre@gmail.com>
2021-03-10 17:55:55 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/alexandrenunes/cursomcatt/CursomcattApplication.java
+++ src/main/java/com/alexandrenunes/cursomcatt/CursomcattApplication.java
@@ -1,5 +1,6 @@
 package com.alexandrenunes.cursomcatt;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.alexandrenunes.cursomcatt.domain.Cidade;
 import com.alexandrenunes.cursomcatt.domain.Cliente;
 import com.alexandrenunes.cursomcatt.domain.Endereco;
 import com.alexandrenunes.cursomcatt.domain.Estado;
+import com.alexandrenunes.cursomcatt.domain.Pagamento;
+import com.alexandrenunes.cursomcatt.domain.PagamentoComBoleto;
+import com.alexandrenunes.cursomcatt.domain.PagamentoComCartao;
+import com.alexandrenunes.cursomcatt.domain.Pedido;
 import com.alexandrenunes.cursomcatt.domain.Produto;
+import com.alexandrenunes.cursomcatt.domain.enums.EstadoPagamento;
 import com.alexandrenunes.cursomcatt.domain.enums.TipoCliente;
 import com.alexandrenunes.cursomcatt.repositories.CategoriaRepository;
 import com.alexandrenunes.cursomcatt.repositories.CidadeRepository;
 import com.alexandrenunes.cursomcatt.repositories.ClienteRepository;
 import com.alexandrenunes.cursomcatt.repositories.EnderecoRepository;
 import com.alexandrenunes.cursomcatt.repositories.EstadoRepository;
+import com.alexandrenunes.cursomcatt.repositories.PagamentoRepository;
+import com.alexandrenunes.cursomcatt.repositories.PedidoRepository;
 import com.alexandrenunes.cursomcatt.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcattApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcattApplication.class, args);
@@ -86,6 +98,22 @@ public class CursomcattApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
 	}
 
