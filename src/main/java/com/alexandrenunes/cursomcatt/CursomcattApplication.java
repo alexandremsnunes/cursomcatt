dd9422f1b9bd1990d33c4c8de54cc805a5134454
alexandremsnunes <msn.alexandre@gmail.com>
2021-03-22 16:02:59 -0300

    Enviando imagem via endpoint

--- src/main/java/com/alexandrenunes/cursomcatt/CursomcattApplication.java
+++ src/main/java/com/alexandrenunes/cursomcatt/CursomcattApplication.java
@@ -1,17 +1,13 @@
 package com.alexandrenunes.cursomcatt;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.alexandrenunes.cursomcatt.services.S3Service;
-
 @SpringBootApplication
 public class CursomcattApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcattApplication.class, args);
@@ -19,6 +15,6 @@ public class CursomcattApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {	
-		s3Service.uploadFile("C:\\Users\\ALEXANDRE\\Documents\\workspace-spring-tool-suite-4-4.9.0.RELEASE\\listesteaa.jpg");
+		
 	}	
 }
