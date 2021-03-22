0bad9d4ca81f10e9d7ca6cfa79d1c2fb82b8939f
alexandremsnunes <msn.alexandre@gmail.com>
2021-03-22 17:07:49 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/alexandrenunes/cursomcatt/domain/Cliente.java
+++ src/main/java/com/alexandrenunes/cursomcatt/domain/Cliente.java
@@ -53,7 +53,7 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
+
 
 	
 	public Cliente() {
@@ -134,15 +134,6 @@ public class Cliente implements Serializable {
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
 	}
-	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
 	
 	@Override
 	public int hashCode() {
